fda5dc4ccd14dab2dbd34f80f7cde46135eb491c
Saem Mohammad Nirob <saemnirob.stat.jnu@gmail.com>
2025-10-22 09:57:02 +0000

    Test Datasets

---
+++ Python_Examples/File_Read_Simulate.py
@@ -0,0 +1,8 @@
+# File Read Simulate.py
+# Simulate reading lines from a file using a list.
+
+lines = ["first line\n", "second line\n", "third line\n"]
+
+if __name__ == '__main__':
+    for idx, line in enumerate(lines,1):
+        print(idx, line.strip())
