5d2cc5371362b28aee0790f722f3f389ab58ee3b
Saem Mohammad Nirob <saemnirob.stat.jnu@gmail.com>
2025-10-22 04:53:28 +0000

    Test  Datasets

---
+++ Tuple/Tuple_Unpacking.py
@@ -0,0 +1,9 @@
+# Tuple Unpacking.py
+# Demonstrates unpacking tuples.
+
+person = ("Alice", 25, "Engineer")
+
+name, age, profession = person
+
+if __name__ == '__main__':
+    print(f"Name: {name}, Age: {age}, Profession: {profession}")
